375ecf46a3393947ee6c415572ac914da8753157
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-11-21 16:03:17 +0100

    testing some react

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log('hello here')
+
+ReactDOM.render(
+  <h1>Dynamic PDP Simulator</h1>,
+  document.getElementById('root')
+);
