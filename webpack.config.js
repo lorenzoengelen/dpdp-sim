9cc550b22834ffba92a005fd11bca0c14d4e4e7f
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-11-21 16:02:09 +0100

    setup webpack dev server in config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
 
-module.exports = {
+const config = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080
   }
 };
+
+module.exports = config;
