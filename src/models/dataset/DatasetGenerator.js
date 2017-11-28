4fb04e62b825937d2fcaea8b89abaead89dcdfff
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-11-28 20:14:31 +0100

    added builder class

--- src/models/dataset/DatasetGenerator.js
+++ src/models/dataset/DatasetGenerator.js
@@ -10,7 +10,46 @@ export default class DatasetGenerator {
     console.log('DatasetGenerator got constructed');
   }
 
-  static builder(name) {
-    console.log('hello' + name);
+  static builder() {
+    console.log('instantiated .builder()');
+    return new Builder();
   }
 };
+
+class Builder {
+  constructor() {
+    console.log('Builder got constructed');
+  }
+
+  setRandomSeed(seed) {}
+
+  setScenarioLength(hours) {
+    return this;
+  }
+
+  setDynamismLevels(levels) {
+    return this;
+  }
+
+  setUrgencyLevels(levels) {
+    return this;
+  }
+
+  setScaleLevels(levels) {
+    return this;
+  }
+
+  setNumInstances(num) {
+    return this;
+  }
+
+  setNumThreads(i) {}
+
+  setDatasetDir(string) {
+    return this;
+  }
+
+  build() {
+    return new DatasetGenerator(this);
+  }
+}
