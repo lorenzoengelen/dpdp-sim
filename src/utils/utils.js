7d14601132932af6e2e72d4ac5d430a0b0a10f31
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-11-29 20:41:23 +0100

    hardcoded the travel speed in utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,6 @@
+const TRAVEL_SPEED = 50; // constant travel speed of 50 km/h
+const MS_IN_HOUR = 3600000;
+
 // EUCLIDEAN DISTANCE
 export function distance(x1, y1, x2, y2) {
   return Math.sqrt(Math.pow(Math.abs(x1 - x2), 2) + Math.pow(Math.abs(y1 - y2), 2));
@@ -5,5 +8,5 @@ export function distance(x1, y1, x2, y2) {
 
 // TRAVEL TIME
 export function tt(x1, y1, x2, y2) {
-
+  return distance(x1, y1, x2, y2) / TRAVEL_SPEED * MS_IN_HOUR;
 };
