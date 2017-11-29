5a6474299da9c04bd16a85dc4a11982dc89020b1
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-11-29 18:36:23 +0100

    added location info

--- src/mdp/order.js
+++ src/mdp/order.js
@@ -1,6 +1,8 @@
 export default class Order {
-  constructor(id, q, e, l, s) {
+  constructor(id, x, y, q, e, l, s) {
     this.id = id;           // id
+    this.x = x;             // x location
+    this.y = y;             // y location
     this.q = q;             // +pickup / -delivery quantity
     this.e = e;             // earliest time
     this.l = l;             // latest time
