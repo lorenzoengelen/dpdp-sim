ca2e4c6d1f15ad8ab18977de43b9db89b5f40a76
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-11-29 20:35:33 +0100

    created utils file for travel distance, travel time, etc.

---
+++ src/utils/utils.js
@@ -0,0 +1,9 @@
+// EUCLIDEAN DISTANCE
+export function distance(x1, y1, x2, y2) {
+  return Math.sqrt(Math.pow(Math.abs(x1 - x2), 2) + Math.pow(Math.abs(y1 - y2), 2));
+};
+
+// TRAVEL TIME
+export function tt(x1, y1, x2, y2) {
+
+};
