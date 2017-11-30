13bef676ff9046f008c6f40ccb853e2322495459
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-11-30 09:42:40 +0100

    note hardcoded loops

--- src/mdp/pfa.js
+++ src/mdp/pfa.js
@@ -10,23 +10,22 @@ export default class PFA {
 
   approximate() {
     // iterate over SAMPLE PATHS
-    for (let h = 0, lnh = this.samples.length; h < 1; h++) { // <== NOTE HARDCODED 1 LOOP ==>
+    for (let h = 0, lnh = this.samples.length; h < 1; h++) { // <== NOTE HARDCODED 1 LOOP (lnh) ==>
       // set REALIZATIONS
       const sample = this.samples[h];
       
       // set INITIAL STATE
-      const initialState = new State(this.init.k, this.init.tk); 
+      const initialState = new State(this.init.k, this.init.tk, this.init.c, this.init.C, this.init.R); 
       const states = [initialState];
 
       // iterate over REALIZATIONS
-      for (let k = 0, lnk = sample.length; k < lnk; k++) {
+      for (let k = 0, lnk = sample.length; k < 1; k++) { // <== NOTE HARDCODED 10 LOOP (lnk) ==>
         const realization = sample[k], r = sample[k];
 
         // PRE-DECISION STATE
         const {C, R} = states[states.length - 1];
         // enter a new DECISION STATE due to new request for service
         const decisionState = new State(r.k, r.a, r, C, R);
-        console.log(decisionState);
 
         // the policy maps the DECISION STATE to a POST-DECISION STATE
         const postDecisionState = this.policy(decisionState);
