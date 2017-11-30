907b62bbe022b9542f1a46861eef65542fef52ae
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-11-30 09:42:18 +0100

    order before refactor

--- src/mdp/order.js
+++ src/mdp/order.js
@@ -1,6 +1,6 @@
 export default class Order {
-  constructor(id, x, y, q, e, l, s) {
-    this.id = id;           // id
+  constructor(id, x, y, q, e, l, s, a, w, b) {
+    this.oid = id;          // id
     this.x = x;             // x location
     this.y = y;             // y location
     this.q = q;             // +pickup / -delivery quantity
@@ -9,5 +9,6 @@ export default class Order {
     this.s = s;             // service time
     this.a = a;             // arrival time
     this.w = w;             // waiting time
+    this.b = b;             // begin servicing
   }
 }
