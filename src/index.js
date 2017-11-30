e79b82733b601cb72da36c2cfedc2a9ae052b5c7
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-11-30 11:59:45 +0100

    new form of initializing pfa

--- src/index.js
+++ src/index.js
@@ -35,45 +35,12 @@ setInterval(tick, 1000);
 // POLICY FUNCTION APPROXIMATION
 import PFA from './mdp/pfa';
 import State from './mdp/state';
+import {Samples, Path, Realization} from './mdp/samples';
 import policy from './mdp/policy';
 
 const ab = s => { return { a: Number(s.split(',')[0]), b: Number(s.split(',')[1]) }; };
 
-// step 0: initialization
-// step 0a: create SAMPLE PATHS
-const samples = [];
-
-for (let i = 0; i < 5; i++) {
-  // iterate over SAMPLE PATHS
-  const sample = require(`./lon/0.80-20-1.00-${i}.scen`);
-  console.log(`SAMPLE PATH: ./lon/0.80-20-1.00-${i}.scen`);
-
-  const realizations = [];
-
-  let k = 0; // decision point k
-  // iterate over SAMPLE REALIZATIONS
-  for (let j = 0, len = sample.events.length; j < len; j++) {
-    const realization = sample.events[j], e = sample.events[j];
-    if (realization.class === 'com.github.rinde.rinsim.pdptw.common.AutoValue_AddParcelEvent') {
-
-      ++k;
-      const a = e.value.time; // announce time
-
-      const pxy = ab(e.value.parcelDTO[0]), px = pxy.a, py = pxy.b; // pickup location (px, py)
-      const p_tw = ab(e.value.parcelDTO[2]), pe = p_tw.a, pl = p_tw.b; // pickup time window (pe, pl)
-      const ps = e.value.parcelDTO[6]; // pickpup service time (ps)
-
-      const dxy = ab(e.value.parcelDTO[1]), dx = dxy.a, dy = dxy.b; // delivery location (dx, dy)
-      const d_tw = ab(e.value.parcelDTO[3]), de = d_tw.a, dl = d_tw.b; // delivery time window (de, dl)
-      const ds = e.value.parcelDTO[7]; // delivery service time (ds)
-      
-      realizations.push({k, a, px, py, dx, dy, pe, pl, de, dl, ps, ds, alpha: null});
-    }
-  } // endfor k (set realizations K)
-  samples.push(realizations);
-} // endfor h (set sample paths H)
-
-// create initial state
+// create INITIAL STATE
 const initialState = State.init()
   .addVehicle(2.5, 2.5)
   .addVehicle(2.5, 2.5)
@@ -84,14 +51,62 @@ const initialState = State.init()
   .addVehicle(7.5, 7.5)
   .addVehicle(7.5, 7.5);
 
-// initialize policy function approximation
+// init SAMPLE PATH REALIZATIONS
+const samples = Samples.init();
+
+for (let i = 0; i < 5; i++) {
+  // iterate over DATA
+  const data = require(`./lon/0.80-20-1.00-${i}.scen`);
+  console.log(`SAMPLE PATH: ./lon/0.80-20-1.00-${i}.scen`);
+
+  // init SAMPLE PATH
+  const path = Path.init();
+
+  let k = 0; // decision point k
+  
+  // iterate over REALIZATIONS
+  for (let j = 0, len = data.events.length; j < len; j++) {
+    const e = data.events[j];
+    if (e.class === 'com.github.rinde.rinsim.pdptw.common.AutoValue_AddParcelEvent') {
+
+      ++k;
+      const a = e.value.time; // announce time
+      const pxy = ab(e.value.parcelDTO[0]), px = pxy.a, py = pxy.b; // pickup location (px, py)
+      const p_tw = ab(e.value.parcelDTO[2]), pe = p_tw.a, pl = p_tw.b; // pickup time window (pe, pl)
+      const ps = e.value.parcelDTO[6]; // pickpup service time (ps)
+      const dxy = ab(e.value.parcelDTO[1]), dx = dxy.a, dy = dxy.b; // delivery location (dx, dy)
+      const d_tw = ab(e.value.parcelDTO[3]), de = d_tw.a, dl = d_tw.b; // delivery time window (de, dl)
+      const ds = e.value.parcelDTO[7]; // delivery service time (ds)
+
+      // init REALIZATION
+      const realization = Realization.init()
+        .setk(k)
+        .setAnnounceTime(a)
+        .setPickupLocation(px, py)
+        .setPickupTimeWindow(pe, pl)
+        .setPickupServiceTime(ps)
+        .setDeliveryLocation(dx, dy)
+        .setDeliveryTimeWindow(de, dl)
+        .setDeliveryServiceTime(ds);
+
+      // add REALIZATION to SAMPLE PATH
+      path.addRealization(realization);
+    }
+  } // endfor k (iterate realization)
+  // add SAMPLE PATH to SAMPLES
+  samples.addPath(path);
+} // endfor h (iterate samples)
+
+// init POLICY FUNCTION APPROXIMATION
 const pfa = PFA.init()
   .setInitialState(initialState)
-  .setPolicy(policy)
+  .setPolicyFunction(policy)
   .setSamples(samples);
 
-// approximate policy
-pfa.approximate();
+console.log(pfa);
+
+// APPROXIMATE policy
+// pfa.approximate();
 
 
 
