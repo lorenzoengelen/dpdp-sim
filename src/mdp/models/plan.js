89406a283b2e325de44c35f82eca35563db6462b
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-11-30 14:20:44 +0100

    when init a route, add home visits to route

--- src/mdp/models/plan.js
+++ src/mdp/models/plan.js
@@ -1,3 +1,9 @@
+import {Pickup, Delivery, Home} from './order';
+
+// <== HARD CODED ==>
+const START_HORIZON = 0;
+const END_HORIZON = 28000000;
+
 export class RoutePlan {
   constructor() {
     this.m = 0; // a fleet of m vehicles
@@ -11,7 +17,7 @@ export class RoutePlan {
       .setHomeLocation(xHomeLocation, yHomeLocation));
     return this;
   }
-};
+}; // endClass RoutePlan
 
 export class Route {
   constructor() {
@@ -33,14 +39,23 @@ export class Route {
   setHomeLocation(x, y) {
     this.xHomeLocation = x;
     this.yHomeLocation = y;
+    // start HOME
+    this.visits.push(Home.init()
+      .setId(0)
+      .setLocation(x, y)
+      .setQuantity(1)
+      .setTimeWindow(START_HORIZON, END_HORIZON)
+      .setServiceTimeDuration(0)
+      .setArrivalTime(0));
+    // end HOME
+    this.visits.push(Home.init()
+      .setId(0)
+      .setLocation(x, y)
+      .setQuantity(-1)
+      .setTimeWindow(START_HORIZON, END_HORIZON)
+      .setServiceTimeDuration(0)
+      .setArrivalTime(0));
+      
     return this;
   }
-
-  addHomeLocations() {
-
-  }
-
-  addVisit(order) {
-
-  }
-};
+}; // endClass Route
