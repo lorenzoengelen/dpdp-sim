acaaa4ed836d387df69db3034e63018553190544
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-12-03 15:54:28 +0100

    determine which route requires an update

--- src/mdp/policy.js
+++ src/mdp/policy.js
@@ -15,16 +15,25 @@ export const myopic = decisionState => {
   // set amount VEHICLES and planned ROUTES
   const {m, routes} = R;
 
-  const routeToUpdate = null;
+  let routeUpdateCost = Infinity;
+  let routeToUpdate = null;
 
   // iterate over ROUTES
   for (let i = 0; i < m; i++) {
     const plannedRoute = routes[i];
     // return new route after CHEAPEST INSERTION
     const newRoute = plannedRoute.cheapestInsertion(c);
-    console.log(newRoute);
+    // if customer is inserted, feasible insertion
+    if (newRoute.getNumberOfCustomerVisits() > plannedRoute.getNumberOfCustomerVisits()) {
+      // change route to update if lower costs
+      if (newRoute.getRouteCost() - plannedRoute.getRouteCost() < routeUpdateCost) {
+        routeUpdateCost = newRoute.getRouteCost() - plannedRoute.getRouteCost();
+        routeToUpdate = newRoute;
+      } // endif routeToUpdate
+    } // endif feasible insertion
+  } // endfor iterate over m routes
 
-  }
+  console.log(routeToUpdate)
 
   return 'post-decision state';
 };
