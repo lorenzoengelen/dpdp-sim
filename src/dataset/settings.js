127fc00a836675888f3beec7fdbf7992d0d28e4f
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-12-14 11:24:19 +0100

    generator settings

--- src/dataset/settings.js
+++ src/dataset/settings.js
@@ -1,6 +1,6 @@
 export default class GeneratorSettings {
   constructor(b) {
-
+    this.builder = b;
   }
 
   static builder() {
@@ -14,36 +14,47 @@ class Builder {
   }
 
   setDayLength(length) {
-    console.log('T', length)
+    this.dayLength = length;
     return this;
   }
 
   setOfficeHours(hours) {
-    console.log('O', hours);
+    this.officeHours = hours;
     return this;
   }
 
   setTimeSeriesType(type) {
+    this.timeSeriesType = type;
     return this;
   }
 
-  setDynamismRangeCenters(map) {
+  setDynamismRange(range) {
+    this.dynamismRange = range;
+    return this;
+  }
+
+  setDynamismRangeCenters(rangeCenter) {
+    this.dynamismRangeCenters = rangeCenter;
     return this;
   }
 
   setUrgency(urgency) {
+    this.urgency = urgency;
     return this;
   }
 
   setScale(scale) {
+    this.scale = scale;
     return this;
   }
 
   setNumOrders(numOrders) {
+    this.numOrders = numOrders;
     return this;
   }
 
   setProperties(props) {
+    this.properties = props;
     return this;
   }
 };
